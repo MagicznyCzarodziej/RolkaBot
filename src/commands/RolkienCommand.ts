68f1c0854742de866f0065ba0361eca621555a06
Magiczny Czarodziej <nexit07@gmail.com>
2020-03-29 17:50:37 +0200

    Add RolkienCommand

---
+++ src/commands/RolkienCommand.ts
@@ -0,0 +1,14 @@
+import { ICommand } from '../Api';
+import { Message, MessageEmbed } from 'discord.js';
+
+export default class HelpCommand implements ICommand {
+  readonly name = 'rolkien';
+  readonly aliases = [] as string[];
+  readonly usage = '';
+  readonly description = '( ͡° ͜ʖ ͡°)';
+  execute(message: Message, args: string[]) {
+    const embed = new MessageEmbed()
+      .setImage('https://i.imgur.com/xmLdvbh.png');
+    message.channel.send(embed);
+  }
+}
